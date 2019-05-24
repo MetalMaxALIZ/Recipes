4bacb3af7cba4db0c4c20b003316951a9ca8fe46
Edu <eduardo.lopez@digital55.com>
2019-05-24 10:57:04 +0200

    mig

--- db/migrate/20190515180015_rename_column_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :chefname, :username
-  end
-end
--- db/migrate/20190516135814_rename_column_recipes.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnRecipes < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :recipes, :chef_id, :user_id
-
-  end
-end
